c60000cb6f110af57485563bacea9e9f66b75dd9
Jonathan Hickford <jonathan.hickford@gmail.com>
2014-06-04 09:58:31 +0100

    Only passing tests and disabled data generator

---
+++ tools/import_test_results.ps1
@@ -0,0 +1,12 @@
+[xml]$testResults = Get-Content "$(APPVEYOR_BUILD_FOLDER)*.junit.xml"
+
+foreach ($testCase in $testResults.testsuites.testsuite.testcase) {
+  echo $testCase.classname
+  echo $testCase.name
+  $status = "Passed"
+  if ($testCase.failure) {
+    echo $testCase.failure.message
+    $status = "Failed"
+  }
+  Add-AppveyorTest -Name "$($testCase.classname).$($testCase.name)" -Framework "tSQLt" -Outcome $status -ErrorMessage $testCase.failure.message
+}
