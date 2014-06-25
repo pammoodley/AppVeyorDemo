227f9c883365e4c20fd2fe25e8b081732cdf772e
Jonathan Hickford <jonathan.hickford@gmail.com>
2014-06-25 11:38:47 +0100

    Cleaned up files

--- tools/import_test_results.ps1
+++ tools/import_test_results.ps1
@@ -1,12 +1,14 @@
-[xml]$testResults = Get-Content "$($env:APPVEYOR_BUILD_FOLDER)\*.junit.xml"
+if (Test-Path "$($env:APPVEYOR_BUILD_FOLDER)\*.junit.xml") {
+  [xml]$testResults = Get-Content "$($env:APPVEYOR_BUILD_FOLDER)\*.junit.xml"
 
-foreach ($testCase in $testResults.testsuites.testsuite.testcase) {
-  echo $testCase.classname
-  echo $testCase.name
-  $status = "Passed"
-  if ($testCase.failure) {
-    echo $testCase.failure.message
-    $status = "Failed"
+  foreach ($testCase in $testResults.testsuites.testsuite.testcase) {
+    echo $testCase.classname
+    echo $testCase.name
+    $status = "Passed"
+    if ($testCase.failure) {
+      echo $testCase.failure.message
+      $status = "Failed"
+    }
+    Add-AppveyorTest -Name "$($testCase.classname).$($testCase.name)" -Framework "tSQLt" -Outcome $status -ErrorMessage $testCase.failure.message
   }
-  Add-AppveyorTest -Name "$($testCase.classname).$($testCase.name)" -Framework "tSQLt" -Outcome $status -ErrorMessage $testCase.failure.message
 }
