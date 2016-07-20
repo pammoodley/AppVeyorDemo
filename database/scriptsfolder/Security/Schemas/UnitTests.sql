96b508d136d9d53cb41330efd349fc5eb4c345fd
pammoodley <pam.moodley@gmail.com>
2016-07-20 20:04:37 +1000

    Update UnitTests.sql

--- database/scriptsfolder/Security/Schemas/UnitTests.sql
+++ database/scriptsfolder/Security/Schemas/UnitTests.sql
@@ -5,3 +5,11 @@ DECLARE @xp int
 SELECT @xp=1
 EXEC sp_addextendedproperty N'tSQLt.TestClass', @xp, 'SCHEMA', N'UnitTests', NULL, NULL, NULL, NULL
 GO
+
+CREATE SCHEMA [UnitTests_OrderByScore]
+AUTHORIZATION [dbo] 
+GO
+DECLARE @xp int
+SELECT @xp=1
+EXEC sp_addextendedproperty N'tSQLt.TestClass', @xp, 'SCHEMA', N'UnitTests_OrderByScore', NULL, NULL, NULL, NULL
+GO
